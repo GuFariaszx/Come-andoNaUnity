3551ad29ff4fee40c1deee8d083e0dbe5906478e
GuFariaszx <zacharingustavo@gmail.com>
2024-11-04 16:54:46 -0300

    feat: Exercicio 06 Feito

---
+++ Assets/Scripts/Exercicio6.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Exercicio6 : MonoBehaviour
+{
+    // Exercício 06
+    //6. (Escolha de personagem) O jogador pode escolher entre o
+    //personagem Guerreiro ou Mago.Exiba "Guerreiro escolhido" se o
+    //jogador selecionar Guerreiro e "Mago escolhido" se selecionar
+    //Mago.
+
+    [SerializeField] bool Guerreiro;
+    [SerializeField] bool Mago;
+    void Start()
+    {
+        if (Guerreiro == true)
+        {
+            print("Guerreiro Escolhido");
+        }
+        else
+        {
+            print("Mago escolhido");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
