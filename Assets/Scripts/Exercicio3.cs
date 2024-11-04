0609f4c31412d82e500025c2ac8865448012dbad
GuFariaszx <zacharingustavo@gmail.com>
2024-11-04 16:15:16 -0300

    feat: Exercicio 03 Feito

--- Assets/Scripts/Exercicio3.cs
+++ Assets/Scripts/Exercicio3.cs
@@ -2,13 +2,35 @@ using UnityEngine;
 
 public class Exercicio3 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Exercicio 3
+    // O jogo possui um power-up : revive; Caso ele pegue, revive com 50% da vida; caso não Game over;
+
+    /*
+     Operadores Lógicos
+     
+     && (e/ and)
+     !! (ou/ or)
+     ! (não/ not)
+     */
+
+    [SerializeField] bool vivoPlayer; //true
+    [SerializeField] bool powerUp; //true
+    double vidaPlayer = 100;
+
     void Start()
     {
-        
+        if(vivoPlayer == false && powerUp == true)
+        {
+            vidaPlayer = vidaPlayer * 0.5;
+            print("Ainda não, vida atual " + vidaPlayer);
+        }
+        else
+        {
+            print("Game Over! ):");
+        }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
--- Assets/Scripts/exercicio2.cs
+++ Assets/Scripts/exercicio2.cs
@@ -1,9 +1,8 @@
 using UnityEngine;
 
-// exercicio 02
-
 public class Exercicio2 : MonoBehaviour
 {
+    // exercicio 02
     // Ao coletar power-up : Power-Up Coletado!; CAso não seja: Nenhum Power-up Coletado!
     [SerializeField] bool PowerUp;
     void Start()
