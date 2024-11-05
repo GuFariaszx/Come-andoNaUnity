a535c8660590ec5f6e6d70ed4c843787672f28a6
GuFariaszx <zacharingustavo@gmail.com>
2024-11-05 14:54:14 -0300

    feat: Exercicio 09 Feito

--- Assets/Scripts/Exercicio8.cs
+++ Assets/Scripts/Exercicio8.cs
@@ -1,14 +1,28 @@
+using System;
+using Unity.Collections;
+using UnityEditor;
 using UnityEngine;
+using static Unity.Burst.Intrinsics.X86;
 
 public class Exercicio8 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Exercicio 08
+    //8. (Contador de horas e dias) Crie um script que em que uma
+    //variável inteira hora seja incrementada de uma unidade a cada 10
+    //segundos e volte a ser 0 quando alcançar o valor 24. Quando
+    //completar um ciclo, incremente uma variável dias e escreva o
+    //número de dias que se passaram no console. (Coloque o código
+    //dentro da função-evento Update).
+
+    [SerializeField] float Hora = 1.0f;
+    [SerializeField] float pulo = 0.1f;
+    float ciclo;
+
     void Start()
-    {
-        
+    {  
+                
     }
 
-    // Update is called once per frame
     void Update()
     {
         
---
+++ Assets/Scripts/Exercicio9.cs
@@ -0,0 +1,42 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Exercicio9 : MonoBehaviour
+{
+    // (Placar final) Crie um script que lê o placar de uma partida entre
+    //time A e time B.Depois, escreva no console qual dos três
+    //resultados possíveis aconteceu: vitória do time A, vitória do time B
+    //ou empate.Se o empate teve mais de 3 pontos para cada lado,
+    //escreva que foi “um empate emocionante”.
+
+    int resultadoValorMax = 5;
+    int resultadoValorMin = 0;
+    int resultadoA;
+    int resultadoB;
+    
+    void Start()
+    {
+        resultadoA = UnityEngine.Random.Range(resultadoValorMax, resultadoValorMin);
+        resultadoB = UnityEngine.Random.Range(resultadoValorMax, resultadoValorMin);
+        print("Palmeiras "+resultadoA+ " a " + resultadoB + " Barcelona");
+        if (resultadoA == resultadoB && resultadoA >= 3 && resultadoB >=3)
+        {
+            print("Um empate emocionante entre Palmeiras e Barcelona!!!");
+        } 
+        if (resultadoA > resultadoB)
+        {
+            print("Vitória do Palmeiras");
+        }
+        if(resultadoA < resultadoB) 
+        {
+            print("Vitória do Barcelona");
+        }
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
