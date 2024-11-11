a64622803aae42978c500a6ab5bab3633ffeb4b7
GuFariaszx <zacharingustavo@gmail.com>
2024-11-11 14:46:59 -0300

    feat: Exercício 08 Feito

---
+++ Assets/Scripts/lacodeRepeticao/Exercício08.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Exercício08 : MonoBehaviour
+{
+    //8. (Reverter uma string) Escreva um programa que inverte uma
+    //string usando um loop for.
+
+    string nome = "Gustavo";
+    string inverter;
+    void Start()
+    {
+        for (int i = nome.Length -1; i >= 0; i--)
+        {
+           char word = nome[i];
+           string letra = word.ToString();
+            inverter += letra;
+            
+        }
+            print("A palavra "+ nome+ " invertido é : "+inverter);    
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
