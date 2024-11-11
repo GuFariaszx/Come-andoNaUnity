42300653063ece0eb0586aa6e93b5730c57569bb
GuFariaszx <zacharingustavo@gmail.com>
2024-11-11 17:20:15 -0300

    feat: Desafio 02 Feito

---
+++ Assets/Scripts/lacodeRepeticao/Desafio02.cs
@@ -0,0 +1,36 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Desafio02 : MonoBehaviour
+{
+    //Faça um algoritmo que receba o dia, o mês e o ano em que uma pessoa nasceu, imprima na tela quantos anos,
+    //meses e dias essa pessoa já viveu.Leve em consideração o ano com 365 dias e o mês com 30 dias.
+
+    [SerializeField] int dia;
+    [SerializeField] int mes;
+    [SerializeField] int ano;
+
+    void Start()
+    {
+        DateTime dataAtual = DateTime.Now;
+        int diaAtual = dataAtual.Day;
+        int mesAtual = dataAtual.Month;
+        int anoAtual = dataAtual.Year;
+
+        int anosVividos = (anoAtual - ano);
+        int mesesVividos = (mesAtual - mes);
+        //int diasVividos = (anoAtual - anoRecebido) * 365 + (mesAtual - mesRecebido) * 30 + (diaAtual - diaRecebido);
+        int diasVividos = (dia - diaAtual);
+
+
+        print("Anos vividos: " + anosVividos + "\n Meses vividos: " + mesesVividos + "\n Dias vividos: " + diasVividos);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
